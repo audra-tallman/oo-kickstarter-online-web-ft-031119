5e9c29973b0bfa6d12946f1202f4a34236451157
Audra Tallman <audi.tallman@gmail.com>
2019-03-27 03:07:10 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,10 @@
 class Backer
-  attr_accessor :name
-  attr_reader :backed_projects
+
+  attr_reader:name
   
   def initialize (name)  
     @name = name
-    @backed_projects = backed projects
+    @backed_projects = []
   end 
     
 end 
